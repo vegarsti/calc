025cce3cb03e53977e63f3960ac4895b6fdeaf9d
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-07-14 07:57:39 +0200

    Parse expression with integer

---
+++ parser/parser_test.go
@@ -0,0 +1,51 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/vegarsti/calc/ast"
+	"github.com/vegarsti/calc/lexer"
+	"github.com/vegarsti/calc/parser"
+)
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5"
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.TokenLiteral not %s. got=%s", "5", literal.TokenLiteral())
+	}
+	checkParserErrors(t, p)
+}
+
+func checkParserErrors(t *testing.T, p *parser.Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
