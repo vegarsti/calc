767c488990214aa24a9bd7d08b4dbcc650c145d7
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-07-15 22:08:24 +0200

    Parse floats

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -38,6 +38,35 @@ func TestIntegerLiteralExpression(t *testing.T) {
 	checkParserErrors(t, p)
 }
 
+func TestFloatLiteralExpression(t *testing.T) {
+	input := "3.14"
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statements. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.FloatLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.FloatLiteral. got=%T", stmt.Expression)
+	}
+	if literal.TokenLiteral() != "3.14" {
+		t.Errorf("literal.TokenLiteral not %s. got=%s", "3.14", literal.TokenLiteral())
+	}
+	checkParserErrors(t, p)
+}
+
 func TestParsingPrefixExpression(t *testing.T) {
 	prefixTest := []struct {
 		input        string
