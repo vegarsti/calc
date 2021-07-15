f566617f1093a2b8e3b796312d9e5c0deed7c2a9
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-07-15 08:30:58 +0200

    Use parser in REPL

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -8,7 +8,7 @@ import (
 	"os/user"
 
 	"github.com/vegarsti/calc/lexer"
-	"github.com/vegarsti/calc/token"
+	"github.com/vegarsti/calc/parser"
 )
 
 const PROMPT = ">> "
@@ -25,10 +25,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
 
